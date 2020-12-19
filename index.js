84c3751de58a6fa085557b9d8865f62709baeac7
Ann Markham <ann.markham@outlook.com>
2020-12-19 00:17:35 -0700

    remove routes, db/database.js and update mysql2  to mysql2/promise

---
+++ index.js
@@ -0,0 +1,24 @@
+// const inquirer = require('inquirer');
+// const cTable = require('console.table');
+//require('./server')
+
+
+
+//prompt for choices at start  -- write prompts and add/update functions as async await functions?
+   //view all depts    -- get code in departmentRoutes -- Display as formatted table
+   //view all roles
+   //view all employees   -- get code in employeeRoutes -- Display as formatted table
+   //add a dept
+        //second prompt -- what is name of the new dept? .then addDepartment() 
+    //add a role
+        //second prompt --what is title for new role? what is salary for new role? .then addRoles() 
+    //add an employee
+        //second prompt -- employee first name? last name? role? manager? .then addEmployee() 
+    //update an employee role
+        //second prompt -- select an employee name/id? select a role?  .then updateEmployee() 
+
+  //write functions for addDepartment, addRoles, addEamploye, and updateEmployee that are called above  
+
+//
+
+
--- server.js
+++ server.js
@@ -1,26 +1,19 @@
-// const express = require('express');
-// const db = require('./db/database');
+// const mysql = require('mysql2/promise');
+//const cTable = require('console.table');
 
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// const apiRoutes = require('./routes/apiRoutes');
-// // Express middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// // Use apiRoutes
-// app.use('/api', apiRoutes);
-
-// // Default response for any other request(Not Found) Catch all other
-// app.use((req, res) => {
-//   res.status(404).end();
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   // Your MySQL username
+//   user: 'root',
+//   // Your MySQL password
+//   password: 'Iforgot1!',
+//   database: 'employeetracker'
 // });
 
-// // Start server after DB connection
-// db.on('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
+// connection.connect(err => {
+//   if (err) throw err;
+//   console.log('connected as id ' + connection.threadId)
 // });
+
+// module.exports = connection;
