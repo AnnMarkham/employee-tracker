4b73de9b7c6e8b8c639de6adc19f0d2ce3824eec
Ann Markham <ann.markham@outlook.com>
2020-12-19 02:02:27 -0700

    add select from * for depts, roles, employees

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
-// const mysql = require('mysql2/promise');
-//const cTable = require('console.table');
+const mysql = require("mysql2/promise");
+const cTable = require("console.table");
 
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   port: 3306,
-//   // Your MySQL username
-//   user: 'root',
-//   // Your MySQL password
-//   password: 'Iforgot1!',
-//   database: 'employeetracker'
-// });
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  // Your MySQL username
+  user: "root",
+  // Your MySQL password
+  password: "Iforgot1!",
+  database: "employeetracker",
+});
 
+connection();
 // connection.connect(err => {
 //   if (err) throw err;
 //   console.log('connected as id ' + connection.threadId)
 // });
 
+// rewrite as async await functions later?
+
+const allDepartments = () => {
+  "SELECT * FROM department";
+};
+allDepartments();
+
+const allRoles = () => {
+  "SELECT * FROM roles";
+};
+allRoles();
+
+const allEmployees = () => {
+  "SELECT * FROM employee";
+};
+allEmployees();
+
 // module.exports = connection;
+module.exports = {
+  allDepartments,
+  allRoles,
+  allEmployees,
+};
